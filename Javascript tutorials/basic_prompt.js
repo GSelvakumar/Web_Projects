94f04c472a1a828d16ae0d8c17c83a88d2a7a753
GSelvakumar <gselvaofficial@outlook.com>
2021-01-31 22:13:48 +0530

    js

--- Javascript tutorials/basic_prompt.js
+++ Javascript tutorials/basic_prompt.js
@@ -1,2 +1,12 @@
 var myName = "Angela";
 var yourName = prompt("What is your name?");
+
+//String Concatenation
+
+var message = "Hello";
+var name = "Selva";
+alert(message + "there, " + name);
+
+//Count the string length
+
+message.length
