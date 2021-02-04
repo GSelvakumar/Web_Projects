830783a987d3faf9aa1af4ea622613276f64ec0e
GSelvakumar <gselvaofficial@outlook.com>
2021-02-04 13:54:12 +0530

    js

---
+++ Javascript tutorials/Love Calculator/index.js
@@ -0,0 +1,6 @@
+prompt("What is your name? ");
+prompt("What is their name? ");
+
+var loveScore = Math.random() * 100; //random from 0 to 1 
+loveScore = Math.floor(loveScore) + 1 //adding 1 to eliminate the 0
+alert("Your love score is: "+loveScore);
