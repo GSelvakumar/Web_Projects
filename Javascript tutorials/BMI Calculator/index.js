6c249c2df50112cc9f07756e477d651dfd0ea868
GSelvakumar <gselvaofficial@outlook.com>
2021-02-06 22:19:27 +0530

    js

--- Javascript tutorials/BMI Calculator/index.js
+++ Javascript tutorials/BMI Calculator/index.js
@@ -2,8 +2,18 @@ function bmiCalculator(weight, height) {
     var x = weight;
     var y = Math.pow(height, 2);
     var result = x / y;
-    return Math.round(result);
+    BMI = Math.round(result);
+
+    if(BMI < 18.5) {
+        return ("Your BMI is "+BMI+", so you are underweight.");
+    }
+    if(BMI >= 18.5 && BMI <= 24.9) {
+        return ("Your BMI is "+BMI+", so you have a normal weight.");
+    }
+    if(BMI > 24.9) {
+        return ("Your BMI is "+BMI+", so you are overweight.");
+    }
+    
 }
 
-var bmi = bmiCalculator(65, 1.8); 
-console.log("Your bmi is, " + bmi);
+bmiCalculator(65, 1.8); 
