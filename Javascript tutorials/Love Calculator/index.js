46514b310864d138016902892a32953ef9122712
GSelvakumar <gselvaofficial@outlook.com>
2021-02-06 20:44:03 +0530

    js

--- Javascript tutorials/Love Calculator/index.js
+++ Javascript tutorials/Love Calculator/index.js
@@ -3,4 +3,11 @@ prompt("What is their name? ");
 
 var loveScore = Math.random() * 100; //random from 0 to 1 
 loveScore = Math.floor(loveScore) + 1 //adding 1 to eliminate the 0
-alert("Your love score is: "+loveScore);
+
+if (loveScore > 70) {
+    alert("Your love score is "+loveScore+" % "+ "You love each other like Kanye loves Kanye.");
+}
+else{
+    alert("Your love score is: "+loveScore);
+}
+
