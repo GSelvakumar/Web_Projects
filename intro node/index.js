0321bdf103f238327a4fe4a0f39b327e130372de
GSelvakumar <gselvaofficial@outlook.com>
2021-03-19 22:12:49 +0530

    node setup done

---
+++ intro node/index.js
@@ -0,0 +1,8 @@
+var superheroes = require("superheroes");
+var supervillains = require("supervillains");
+
+var mySuperheroName = superheroes.random();
+var mySupervillainsName = supervillains.random();
+
+console.log("My hero name is "+mySuperheroName);
+console.log("My Villain name is "+mySupervillainsName);
